cfe3b9ba95298d5122643b45618a97657140baa1
Bassem Ghoniem <bassem.ghoniem@yahoo.com>
2017-04-05 14:51:00 +0200

    Done! ch3: Asynchronous Control Flow Patterns with Callbacks squential, parallel, and limited parallel async control flow using pure JS and Async

---
+++ ch3/spider6-parrallel-async.js
@@ -0,0 +1,101 @@
+'use strict';
+
+const request = require('request');
+const fs = require('fs');
+const mkdirp = require('mkdirp');
+const path = require('path');
+const utilities = require('./utilities');
+const async = require('async');
+
+const spidering = new Map();
+
+console.dir((new Date()).toTimeString());
+spider(process.argv[2], 1, (err) => {
+  if (err) {
+    console.log(err);
+    console.dir((new Date()).toTimeString());
+    process.exit(1);
+  } else {
+    console.log('Download complete');
+    console.dir((new Date()).toTimeString());
+  }
+});
+
+function spider(url, nesting, callback) {
+  const filename = utilities.urlToFilename(url);
+  if (spidering.has(url)) {
+    return process.nextTick(callback);
+  }
+  spidering.set(url, true);
+
+  return fs.readFile(filename, 'utf8', (err, body) => {
+    if (err) {
+      if (err.code !== 'ENOENT') {
+        return callback(err);
+      }
+
+      return download(url, filename, (err, data) => {
+        if (err) {
+          return callback(err);
+        }
+        return spiderLinks(url, data, nesting, callback);
+      });
+    }
+
+    return spiderLinks(url, body, nesting, callback);
+  });
+}
+
+
+function spiderLinks(currentUrl, body, nesting, callback) {
+  if (nesting === 0) {
+    return process.nextTick(callback);
+  }
+
+  const links = utilities.getPageLinks(currentUrl, body);
+  if (links.length === 0) {
+    return process.nextTick(callback);
+  }
+
+  return async.each(links, (link, cb) => {
+    spider(link, nesting - 1, cb);
+  }, callback);
+}
+
+function download(url, filename, callback) {
+  console.log(`Downloading ${url}`);
+  async.waterfall(
+    [
+      (cb) => {
+        request(url, (err, response, resBody) => {
+          if (err) {
+            return cb(err);
+          }
+          return cb(null, resBody);
+        });
+      },
+      (data, cb) => {
+        mkdirp(path.dirname(filename), (err) => {
+          if (err) {
+            return callback(err);
+          }
+          return cb(null, data);
+        });
+      },
+      (data, cb) =>
+        fs.writeFile(filename, data, (err) => {
+          if (err) {
+            return cb(err);
+          }
+          return cb(null, data);
+        })
+    ],
+    (err, body) => {
+      if (err) {
+        return callback(err);
+      }
+      console.log(`Downloaded and saved: ${url}`);
+      return callback(null, body);
+    }
+  );
+}
---
+++ ch3/spider7-limited-parallel-async.js
@@ -0,0 +1,99 @@
+'use strict';
+
+const request = require('request');
+const fs = require('fs');
+const mkdirp = require('mkdirp');
+const path = require('path');
+const utilities = require('./utilities');
+const async = require('async');
+
+const downloadQueue = async.queue((taskData, callback) => {
+  spider(taskData.link, taskData.nesting - 1, callback);
+}, 2);
+
+const spidering = new Map();
+
+console.dir((new Date()).toTimeString());
+spider(process.argv[2], 1, (err) => {
+  if (err) {
+    console.log(err);
+    process.exit();
+  } else {
+    console.log('Download complete');
+    console.dir((new Date()).toTimeString());
+  }
+});
+
+function spider(url, nesting, callback) {
+  if (spidering.has(url)) {
+    return process.nextTick(callback);
+  }
+  spidering.set(url, true);
+  const filename = utilities.urlToFilename(url);
+  return fs.readFile(filename, 'utf8', (err, body) => {
+    if (err) {
+      if (err.code !== 'ENOENT') {
+        return callback(err);
+      }
+
+      return download(url, filename, (err, data) => {
+        if (err) {
+          return callback(err);
+        }
+        return spiderLinks(url, data, nesting, callback);
+      });
+    }
+
+    return spiderLinks(url, body, nesting, callback);
+  });
+}
+
+
+function spiderLinks(currentUrl, body, nesting, callback) {
+  if (nesting === 0) {
+    return process.nextTick(callback);
+  }
+  const links = utilities.getPageLinks(currentUrl, body);
+  if (links.length === 0) {
+    return process.nextTick(callback);
+  }
+  let completed = 0;
+  let hasErrors = false;
+  return links.forEach((link) => {
+    const taskData = { link, nesting };
+    downloadQueue.push(taskData, (err) => {
+      if (err) {
+        hasErrors = true;
+        return callback(err);
+      }
+      if (++completed === links.length && !hasErrors) {
+        return callback();
+      }
+    });
+  });
+}
+
+function saveFile(filename, contents, callback) {
+  mkdirp(path.dirname(filename), (err) => {
+    if (err) {
+      return callback(err);
+    }
+    return fs.writeFile(filename, contents, callback);
+  });
+}
+
+function download(url, filename, callback) {
+  console.log(`Downloading ${url}`);
+  request(url, (err, response, body) => {
+    if (err) {
+      return callback(err);
+    }
+    return saveFile(filename, body, (err) => {
+      if (err) {
+        return callback(err);
+      }
+      console.log(`Downloaded and saved: ${url}`);
+      return callback(null, body);
+    });
+  });
+}
