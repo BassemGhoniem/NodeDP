6006bb28deb398a14d428802310d26e0672d71ed
Bassem Ghoniem <bassem.ghoniem@yahoo.com>
2017-04-11 12:04:04 +0200

    Done Ch04: Asynchronous Control Flow Patterns with ES2015 and Beyond

---
+++ ch3&4/async-await.js
@@ -0,0 +1,22 @@
+/**
+ * Created by bassem on 4/11/17.
+ */
+
+'use strict';
+
+const request = require('request');
+
+function getPageHtml(url) {
+  return new Promise((resolve) => {
+    request(url, (error, response, body) => {
+      resolve(body);
+    });
+  });
+}
+async function main() {
+  const html = await getPageHtml('http://google.com');
+  console.log(html);
+}
+main();
+
+console.log('Loading...');
